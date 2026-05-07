714f8047c434219ddd86c1b54a28e8dadc73f1cf
catiaeira <catia.eira71@gmail.com>
2026-05-07 13:58:44 +0100

    symbol table improvements

--- symbol_table.py
+++ symbol_table.py
@@ -20,6 +20,8 @@ class SymbolTable:
         self.__label_scopes = [{}]
         # labels are NOT global assets, so we keep them in scopes as well
 
+        self.errors = []
+
     # ---- VAR SCOPES MANAGEMENT ----
 
     def push_var_scope(self):
@@ -27,7 +29,7 @@ class SymbolTable:
 
     def pop_var_scope(self):
         if len(self.__variable_scopes) == 1:
-            raise SemanticError("Cannot pop global variable scope")
+            self.errors.append(SemanticError("Cannot pop global variable scope"))
         self.__variable_scopes.pop()
 
     def current_var_scope(self):
@@ -40,7 +42,7 @@ class SymbolTable:
 
     def pop_label_scope(self):
         if len(self.__label_scopes) == 1:
-            raise SemanticError("Cannot pop global label scope")
+            self.errors.append(SemanticError("Cannot pop global label scope"))
         self.__label_scopes.pop()
 
     def current_label_scope(self):
@@ -53,7 +55,7 @@ class SymbolTable:
         scope = self.current_var_scope()
 
         if id in scope:
-            raise SemanticError(f"Duplicate declaration of variable: {id}")
+            self.errors.append(SemanticError(f"Duplicate declaration of variable: {id}"))
       
         scope[id] = {
             "initialized": False,
@@ -66,7 +68,7 @@ class SymbolTable:
         for scope in reversed(self.__variable_scopes):
             if id in scope:
                 return scope.get(id)
-        raise SemanticError(f"Undeclared variable: {id}")
+        self.errors.append(SemanticError(f"Undeclared variable: {id}"))
 
     # mark a variable as initialized, but only if declared
     def initialize(self, id):
@@ -81,23 +83,23 @@ class SymbolTable:
 
     # ---- FUNCTIONS / SUBROUTINES ----
 
-    # declare a function
-    def declare_fun(self, id, params=None, ret_type=None):
+    # declare a program, function or subroutine
+    def declare_routine(self, id, fkind, params=None, ret_type=None):
         if id in self.__functions:
-            raise SemanticError(f"Duplicate declaration of function/subroutine: {id}")
+            self.errors.append(SemanticError(f"Duplicate declaration of routine: {id}"))
 
         if params is None: params = []
 
         self.__functions[id] = {
-            "kind": "function", # not sure if we'll need to differentiate between func, subroutine, program(?)
+            "kind": fkind, # not sure if we'll need to differentiate between func, subroutine, program(?)
             "parameters": params,
             "return_type": ret_type
         }
 
     # return function info, but only if declared
-    def lookup_fun(self, id):
+    def lookup_routine(self, id):
         if id not in self.__functions:
-            raise SemanticError(f"Undeclared function: {id}")
+            self.errors.append(SemanticError(f"Undeclared routine: {id}"))
         return self.__functions.get(id)
     
     # Semantic constraints on functions:
@@ -106,23 +108,24 @@ class SymbolTable:
     # (3) types of the arguments used must match function parameter types
     # (4) return type must be compatible
 
-    def check_fun_call(self, id, params, eval_expr):
-        func = self.lookup_fun(id) # already raises an error if the function is undeclared # (1)
+    def check_fun_call(self, id, args, eval_expr):
+        func = self.lookup_routine(id) # already raises an error if the function is undeclared # (1)
 
         expected_len = len(func["parameters"])
-        given_len = len(params)
+        given_len = len(args)
 
         if expected_len != given_len:
-            raise SemanticError(f"Function '{id}': expected {expected_len} arguments, got {given_len}") # (2)
+            self.errors.append(SemanticError(f"Function '{id}': expected {expected_len} arguments, got {given_len}")) # (2)
 
-        for arg_node, param in zip(func["parameters"], params):
+        for arg_node, param in zip(args, func["parameters"]):
             arg_type = eval_expr(arg_node)
             param_type = param["type"]
     
             if arg_type != param_type:
-                raise SemanticError(f"Type mismatch in call to '{id}': expected {param_type}, got {arg_type}") # (3)
+                self.errors.append(SemanticError(f"Type mismatch in call to '{id}': expected {param_type}, got {arg_type}")) # (3)
     
-        return func["return_type"] # constraint (4) should be checked by the caller
+        return func["kind"], func["return_type"] # constraint (4) should be checked by the caller
+        # return (kind, return_type) so that the caller can differentiate between functions and subroutines and check the return type
 
     # ---- LABELS ----
     
@@ -154,7 +157,7 @@ class SymbolTable:
 
         if label in scope:
             if scope[label]["defined"]:
-                raise SemanticError(f"Duplicate definition of label: {label}")
+                self.errors.append(SemanticError(f"Duplicate definition of label: {label}"))
             scope[label]["defined"] = True
         else:
             scope[label] = {
@@ -166,25 +169,40 @@ class SymbolTable:
         scope = self.current_label_scope()
 
         if label not in scope or not scope[label]["defined"]:
-            raise SemanticError(f"Undefined label: {label}")
+            self.errors.append(SemanticError(f"Undefined label: {label}"))
 
         return scope[label]
 
     # should be used in a posterior sweep to make sure all declared labels are defined
     def check_undefined_labels(self):
-        scope = self.current_label_scope()
-
-        for label, entry in scope.items():
-            if entry["declared"] and not entry["defined"]:
-                raise SemanticError(
-                    f"Label referenced but never defined: {label}"
-                )
+        for scope in self.__label_scopes:
+            for label, info in scope.items():
+                if info.get("declared") and not info.get("defined"):
+                    self.errors.append(SemanticError(
+                        f"Label referenced but never defined: {label}"
+                    ))
             
     # ---- DEBUG ----
 
     def dump(self):
-        print("=== SYMBOL TABLE ===")
-        for i, scope in enumerate(self.scopes):
-            print(f"Scope {i}: {scope}")
-        print("Functions:", self.functions)
-        print("Labels:", self.labels)
+        print("\n=== SYMBOL TABLE ===")
+
+        for i, scope in enumerate(self.__variable_scopes):
+            print(f"\nVariable Scope {i}:")
+            for name, info in scope.items():
+                print(f"  {name}: {info}")
+
+        print("\nFunctions & Subroutines:")
+        for name, info in self.__functions.items():
+            print(f"  {name}: {info}")
+
+        for i, scope in enumerate(self.__label_scopes):
+            print(f"\nLabel Scope {i}:")
+            for label, info in scope.items():
+                status = []
+                if info.get("declared"):
+                    status.append("declared")
+                if info.get("defined"):
+                    status.append("defined")
+
+                print(f"  {label}: {', '.join(status) if status else 'empty'}")
