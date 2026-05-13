6717acb77c999e5f81a41b8f7646d7d4a054a39f
MarcoFerreira05 <a106857@alunos.uminho.pt>
2026-05-13 16:24:31 +0100

    Add data types to the symbol table

--- code_gen_symbol_table.py
+++ code_gen_symbol_table.py
@@ -1,15 +1,16 @@
 class CodeGenSymbolTable:
     
     def __init__(self):
-        self.variable_dict: dict[str, int] = {}
+        self.variable_dict = {}
         self.global_index: int = 0
 
-    def insert(self, name: str) -> int:
+    def insert(self, name: str, data_type: str) -> int:
         """ Returns the index for the inserted variable. """
         index = self.global_index
-        self.variable_dict[name] = index
+        self.variable_dict[name] = (index, data_type)
         self.global_index += 1
         return index
 
-    def lookup(self, name:str) -> int:
+    def lookup(self, name:str):
+        """ Returns a tuple of the form (index, data_type). """
         return self.variable_dict[name]
